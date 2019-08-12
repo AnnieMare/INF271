31fa7a61ada613883878e74f8421cd9938ae3ee7
AnnieMare <mareannie@gmail.com>
2019-08-12 12:23:00 +0200

    Interfaces Search Passenger & CRUD Booking status

--- NokaAdventures/NokaAdventures/Program.cs
+++ NokaAdventures/NokaAdventures/Program.cs
@@ -16,7 +16,13 @@ namespace NokaAdventures
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BookingStatuses());
+
+           
+
+            
+
         }
+        
     }
 }
